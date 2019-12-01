34b8b5ac6d7d6a56f057ea86656fc48019f6795c
milossimic <milossimicsimo@gmail.com>
2019-12-01 16:08:18 +0100

    added exmaple of keys

--- storage/etcd/helper.go
+++ storage/etcd/helper.go
@@ -11,6 +11,22 @@ const (
 	sep   = "/"
 )
 
+/*
+
+Trace key examples:
+trace/123sdfgdfhgfg/1 -> span with id 1
+trace/123sdfgdfhgfg/1/1 -> child span with id 1 of span with id 1
+trace/123sdfgdfhgfg/1/2
+trace/123sdfgdfhgfg/2
+
+Trace tags query key example:
+trace/tags/123sdfgdfhgfg/1 -> span with id 1
+trace/tags/123sdfgdfhgfg/1/1 -> child span with id 1 of span with id 1
+trace/tags/123sdfgdfhgfg/1/2
+trace/tags/123sdfgdfhgfg/2
+
+*/
+
 func traceKey(traceId string) string {
 	s := []string{trace, traceId}
 	return strings.Join(s, "/")
